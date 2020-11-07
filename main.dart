543b79e9acbc0efa34cfc3db3f0733a37d9421c6
huynhquang23032000 <73038802+huynhquang23032000@users.noreply.github.com>
2020-11-07 11:18:49 +0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,117 +1,91 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'dart:ui' as ui;
 
-import 'package:flutter_app/login_screen.dart';
-void main(){
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Homepage(),
-    )
-  );
+void main() => runApp(MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.blue,
+        accentColor: Colors.orange,
+      ),
+      home: MyApp(),
+    ));
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class Homepage extends StatelessWidget {
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
+  List todos = List();
+  String input = "";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    todos.add("Item1");
+    todos.add("Item2");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Container(
-          margin: EdgeInsets.all(50.0),
-          width:double.infinity,
-          height: MediaQuery.of(context).size.height,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Column(
-                  children: <Widget> [
-                  Text("HELLO!",style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 60,
-                    foreground: Paint()
-                    ..shader= ui.Gradient.linear(
-                      const Offset(0, 20),
-                      const Offset(300,20),
-                      <Color>[
-                        Color(0xcc00ffff),
-                        Color(0xff00cccc),
-                      ],
-                    )
-                  ),
-                  ),
-                    SizedBox(height:20,),
-                    Text("Good to see you here", textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.grey[700],
-                      fontSize: 15
-                    ),
-                    ),
-                  ],  
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height/3,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('lib/img/Login_pics.png')
-                  )
-                ),
-              ),
-              Column(
-                children: <Widget>[
-                  MaterialButton(
-                    minWidth: double.infinity,
-                    height: 60,
-                    onPressed: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginScreen()));
-                    },
-                    shape: RoundedRectangleBorder(
-                      side: BorderSide(
-                        color: Colors.black,
-                      ),
-                      borderRadius: BorderRadius.circular(50)
-                    ),
-                    child: Text("Login",style:TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 18
-                    ),),
-                  ),
-                  SizedBox(height: 20,),
-                  Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                        border: Border(
-                        bottom: BorderSide(color: Colors.black),
-                        top: BorderSide(color: Colors.black),
-                        left: BorderSide(color: Colors.black),
-                        right: BorderSide(color: Colors.black),
-                      )
-                    ),
-                    child:
-                    MaterialButton(
-                      minWidth: double.infinity,
-                      height: 60,
-                      onPressed: ( ){},
-                      color: Colors.cyan,
-                      elevation: 0,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50)
-                      ),
-                      child: Text("Sign up",style:TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 18
-                      ),),
-                    ),
-                  )
-                ]
-              )
-            ],
-          )
-        )
+      appBar: AppBar(
+        title: Text("mytodos"),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  title: Text("Them vao Todolist "),
+                  content: TextField(
+                    onChanged: (String value) {
+                      input = value;
+                    },
+                  ),
+                  actions: <Widget>[
+                    FlatButton(
+                        onPressed: () {
+                          setState(() {
+                            todos.add(input);
+                          });
+                          Navigator.of(context).pop();
+                        },
+                        child: Text("Them"))
+                  ],
+                );
+              });
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+      body: ListView.builder(
+          itemCount: todos.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Dismissible(
+                key: Key(todos[index]),
+                child: Card(
+                  elevation: 4,
+                  margin: EdgeInsets.all(8),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16)),
+                  child: ListTile(
+                    title: Text(todos[index]),
+                    trailing: IconButton(
+                        icon: Icon(Icons.delete, color: Colors.red),
+                        onPressed: () {
+                          setState(() {
+                            todos.removeAt(index);
+                          });
+                        }),
+                  ),
+                ));
+          }),
     );
   }
 }
